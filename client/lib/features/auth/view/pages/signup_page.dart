b8b8fe04c396fb610706510cce6465cdf9281279
W ADITYA <aditya.23bce8002@vitapstudent.ac.in>
2025-08-02 12:04:15 +0530

    Created SignUp Page

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -11,38 +11,73 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+    formKey.currentState!.validate(); 
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Raagam',
-              style: TextStyle(
-                fontSize: 50, 
-                fontWeight: FontWeight.bold,
-                color: Pallete.gradient2,
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Raagam',
+                style: TextStyle(
+                  fontSize: 50, 
+                  fontWeight: FontWeight.bold,
+                  color: Pallete.gradient2,
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
-            CustomField(hintText: 'Name'),
-            const SizedBox(height: 15),
-            CustomField(hintText: 'Email'),
-            const SizedBox(height: 15),
-            CustomField(hintText: 'Password'),
-            const SizedBox(height: 15),
-            AuthGradientButton(),
-            const SizedBox(height: 15),
-            RichText(
-              text: TextSpan(
-                text: 'Already have an account ?'
+              const SizedBox(height: 30),
+              CustomField(
+                hintText: 'Name', 
+                controller: nameController
               ),
-            ),
-          ],
+              const SizedBox(height: 15),
+              CustomField(
+                hintText: 'Email', 
+                controller: emailController
+              ),
+              const SizedBox(height: 15),
+              CustomField(
+                hintText: 'Password', 
+                controller: passwordController,
+                obscureText: true,
+              ),
+              const SizedBox(height: 15),
+              AuthGradientButton(),
+              const SizedBox(height: 15),
+              RichText(
+                text: TextSpan(
+                  text: 'Already have an account ?',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: ' Sign In',
+                      style: const TextStyle(
+                        color: Pallete.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- client/lib/features/auth/view/widgets/custom_field.dart
+++ client/lib/features/auth/view/widgets/custom_field.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
   final String hintText;
-  const CustomField({super.key, required this.hintText});
+  final TextEditingController controller;
+  final bool obscureText;
+  const CustomField({super.key, required this.hintText, required this.controller, this.obscureText = false});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
       ),
+      validator: (value) {
+        if (value!.isEmpty){
+          return "$hintText is missing";
+        }
+        return null;
+      },
+      obscureText: obscureText,
+      obscuringCharacter: '*',
     );
   }
 }
