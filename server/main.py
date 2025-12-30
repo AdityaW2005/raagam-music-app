c062e3524002d5ce2807e8481341b3dd206dc58e
AdityaW2005 <adhiw2005@gmail.com>
2025-12-30 18:48:15 +0530

    Add .gitignore file and insert a blank line in main.py

--- server/main.py
+++ server/main.py
@@ -8,6 +8,7 @@ DATABASE_URL = 'postgresql://postgres:musicapp123@localhost:5433/fluttermusicapp
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 db = SessionLocal()
+
 class UserCreate(BaseModel):
     name: str
     email: str
