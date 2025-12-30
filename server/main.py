1a42553c0ffe2b7b55a8e15c38de4a039e636f9f
AdityaW2005 <adhiw2005@gmail.com>
2025-12-30 13:43:28 +0530

    Refactor database session creation in FastAPI application

--- server/main.py
+++ server/main.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import sessionmaker
 app = FastAPI()
 DATABASE_URL = 'postgresql://postgres:musicapp123@localhost:5433/fluttermusicapp'
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit = False)
-
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+db = SessionLocal()
 class UserCreate(BaseModel):
     name: str
     email: str
