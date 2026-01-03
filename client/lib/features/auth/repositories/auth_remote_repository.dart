94381b4cf6e159789e6839a61aae927b581e3953
AdityaW2005 <adhiw2005@gmail.com>
2026-01-03 21:54:20 +0530

    Refactor login method to return Either<AppFailure, UserModel> and improve error handling

--- client/lib/features/auth/repositories/auth_remote_repository.dart
+++ client/lib/features/auth/repositories/auth_remote_repository.dart
@@ -34,17 +34,25 @@ class AuthRemoteRepository {
     }
   }
 
-  Future<void> login({required String email, required String password}) async {
+  Future<Either<AppFailure, UserModel>> login({
+    required String email,
+    required String password,
+  }) async {
     try {
       final response = await http.post(
         Uri.parse('$_baseUrl/auth/login'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({'email': email, 'password': password}),
       );
-      print(response.body);
-      print(response.statusCode);
+
+      final resBodyMap = jsonDecode(response.body) as Map<String, dynamic>;
+      if (response.statusCode != 200) {
+        return Left(AppFailure(resBodyMap['detail']));
+      }
+
+      return Right(UserModel.fromMap(resBodyMap));
     } catch (e) {
-      print(e);
+      return Left(AppFailure(e.toString()));
     }
   }
 }
