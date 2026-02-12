789a9321446bcb180125617e67812876b3157f25
AdityaW2005 <adhiw2005@gmail.com>
2026-02-12 19:26:02 +0530

    feat: Implement current user data retrieval and enhance error handling for token verification

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,13 +1,14 @@
 import uuid
 import bcrypt
 import jwt
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Header
 from fastapi import APIRouter
 from server.models.user import User
 from server.pydantic_schemas.user_create import UserCreate
 from server.database import get_db
 from sqlalchemy.orm import Session
 from server.pydantic_schemas.user_login import UserLogin
+from typing import Optional
 
 router = APIRouter()
 
@@ -40,7 +41,7 @@ def login_user(user: UserLogin, db: Session = Depends(get_db)):
     # Check if a user with the same email already exists
     user_db = db.query(User).filter(User.email == user.email).first()
     if not user_db:
-        raise HTTPException(400, "User with the same email already exists!")
+        raise HTTPException(400, "User not found!")
     
     # Password matching or not
     is_match = bcrypt.checkpw(user.password.encode(), user_db.password)
@@ -51,3 +52,25 @@ def login_user(user: UserLogin, db: Session = Depends(get_db)):
     token = jwt.encode({'id': user_db.id}, 'password_key')
 
     return {'token': token, 'user': user_db}
+
+@router.get('/')
+def current_user_data(db: Session = Depends(get_db), x_auth_token = Header()):
+    try:
+        # get the user token from the headers
+        if not x_auth_token:
+            raise HTTPException(401, "No auth token, Access denied!")
+
+        # decode the token
+        verified_token = jwt.decode(x_auth_token, "password_key", ["HS256"])
+
+        if not verified_token:
+            raise HTTPException(401, "Token verification failed for authorization")
+        
+        # get the id from token
+        uid = verified_token.get('id')
+        return uid
+
+        # postgres db get the user info
+
+    except jwt.PyJWTError:
+        raise HTTPException(401, "Token is invalid, authorization failed!")
